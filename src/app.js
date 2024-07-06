130309096608bea74f91d05e0c39ef1632f5751f
HemantSuthar7 <hemantsuthar9324@gmail.com>
2024-07-06 15:08:30 +0530

    added ApiResponse, ApiError & asyncHandler utilities

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from 'cors';
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit:"16kb"})) //TO limit the amount of json data coming to the server
+
+app.use(express.urlencoded({extended: true, limit:"16kb"}))  //the configure the URL encoding from the params
+
+app.use(express.static("public")) // to store static files like images, documents, etc in the server itself
+
+app.use(cookieParser()) // to successfully parse the cookies 
+
+
+export {app};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-
+import { app } from "./app.js"
 import connectDB from "./db/index.js"
 import dotenv from "dotenv"
 
@@ -10,6 +10,19 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("ERROR",error)
+        throw error
+    })
+    app.listen(process.env.PORT || 4000, ()=>{
+        console.log(`Server is running on port ${process.env.PORT}`)
+    })
+    
+})
+.catch((err)=>{
+    console.log("MonogoDB connection failed !!!",err)
+})
 
 
 
