5da40ed6c4bd08a5631be55587c66103a0597f04
HemantSuthar7 <hemantsuthar9324@gmail.com>
2024-07-08 20:33:02 +0530

    Project Setup configured and tested user router

--- src/app.js
+++ src/app.js
@@ -10,12 +10,20 @@ app.use(cors({
 }))
 
 app.use(express.json({limit:"16kb"})) //TO limit the amount of json data coming to the server
-
 app.use(express.urlencoded({extended: true, limit:"16kb"}))  //the configure the URL encoding from the params
-
 app.use(express.static("public")) // to store static files like images, documents, etc in the server itself
-
 app.use(cookieParser()) // to successfully parse the cookies 
+
+
+
+// Import Routes
+
+import userRouter from "./routes/user.routes.js";
+
+
+// Routes Declaration
+
+app.use("/api/v1/users", userRouter)
 
 
 export {app};
